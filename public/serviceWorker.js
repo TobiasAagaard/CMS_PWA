5db88f705e2419dad04f9532a9e4b5e9fc5900bb
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-13 10:01:58 +0100

    Offline html

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,30 +1,36 @@
-const CACHE_NAME = "version_1"
+const CACHE_NAME = "version-1";
+const urlsToCache = ["../index.html", "offline.html"];
+const self = this;
 
-const urlsToCache = ["index.html", "offline.html" ]
-
-this.addEventListener('install', (event) => {
+//installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-        console.log("opend chache")
-        return cache.addAll(urlsToCache);
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
     })
-})
+  );
+});
 
-this.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((res) => {
-            return fetch(event.request).catch(() => caches.match('offline.html'))
-        })
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
     )
-})
-
-this.addEventListener('activate', (event) => {
-    const cacheWhiteList = []
-    cacheWhiteList.push(CACHE_NAME)
-    event.waitUntil(caches.keys().then((cacheNames) => Promise.all(
-        cachesNames.map((cacheNames) => {
-            if(!cacheWhiteList.includes(cacheNames)) {
-                return cache.delete(cacheNcacheNamesames)
-            }
-        })
-    ) ))
-})
+});
