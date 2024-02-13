fc2524691153df2e82782b1bbe92800018999092
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-13 11:36:42 +0100

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "version-1";
 const urlsToCache = ["../index.html", "offline.html"];
 const self = this;
 
-//installation
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
